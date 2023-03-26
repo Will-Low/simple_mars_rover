8c1e7eb795880e4cbb7dd77cd81cfe71bb775ce5
Will-Low <26700668+Will-Low@users.noreply.github.com>
2023-03-25 22:16:23 -0700

    Updating test names

--- rover/rover_test.go
+++ rover/rover_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func assertCommandYieldsPositionOnGrid(t *testing.T, command string, expectedPosition string, grid Grid) {
+func assertThatCommandYieldsPositionOnGrid(t *testing.T, command string, expectedPosition string, grid Grid) {
 	// Arrange
 	rover := NewRover(grid)
 
@@ -20,64 +20,64 @@ func assertCommandYieldsPositionOnGrid(t *testing.T, command string, expectedPos
 var tenByTenGrid = NewGrid(10, 10)
 
 func TestWhenTheRoverIsNotMovedThenItIsAtTheStartingPosition(t *testing.T) {
-	assertCommandYieldsPositionOnGrid(t, "", "0:0:N", tenByTenGrid)
+	assertThatCommandYieldsPositionOnGrid(t, "", "0:0:N", tenByTenGrid)
 }
 
 func TestWhenTheRoverIsRotatedLeftOneTimeThenItFacesWest(t *testing.T) {
-	assertCommandYieldsPositionOnGrid(t, "L", "0:0:W", tenByTenGrid)
+	assertThatCommandYieldsPositionOnGrid(t, "L", "0:0:W", tenByTenGrid)
 }
 
 func TestWhenTheRoverIsRotatedLeftTwoTimesThenItFacesSouth(t *testing.T) {
-	assertCommandYieldsPositionOnGrid(t, "LL", "0:0:S", tenByTenGrid)
+	assertThatCommandYieldsPositionOnGrid(t, "LL", "0:0:S", tenByTenGrid)
 }
 func TestWhenTheRoverIsRotatedLeftThreeTimesThenItFacesEast(t *testing.T) {
-	assertCommandYieldsPositionOnGrid(t, "LLL", "0:0:E", tenByTenGrid)
+	assertThatCommandYieldsPositionOnGrid(t, "LLL", "0:0:E", tenByTenGrid)
 }
 
 func TestWhenTheRoverIsRotatedLeftFourTimesThenItFacesNorth(t *testing.T) {
-	assertCommandYieldsPositionOnGrid(t, "LLLL", "0:0:N", tenByTenGrid)
+	assertThatCommandYieldsPositionOnGrid(t, "LLLL", "0:0:N", tenByTenGrid)
 }
 
 func TestWhenTheRoverIsRotatedRightOneTimeThenItFacesEast(t *testing.T) {
-	assertCommandYieldsPositionOnGrid(t, "R", "0:0:E", tenByTenGrid)
+	assertThatCommandYieldsPositionOnGrid(t, "R", "0:0:E", tenByTenGrid)
 }
 func TestWhenTheRoverIsRotatedRightTwoTimesThenItFacesSouth(t *testing.T) {
-	assertCommandYieldsPositionOnGrid(t, "RR", "0:0:S", tenByTenGrid)
+	assertThatCommandYieldsPositionOnGrid(t, "RR", "0:0:S", tenByTenGrid)
 }
 
 func TestWhenTheRoverIsRotatedRightThreeTimesThenItFacesWest(t *testing.T) {
-	assertCommandYieldsPositionOnGrid(t, "RRR", "0:0:W", tenByTenGrid)
+	assertThatCommandYieldsPositionOnGrid(t, "RRR", "0:0:W", tenByTenGrid)
 }
 
 func TestWhenTheRoverIsRotatedRightFourTimesThenItFacesNorth(t *testing.T) {
-	assertCommandYieldsPositionOnGrid(t, "RRRR", "0:0:N", tenByTenGrid)
+	assertThatCommandYieldsPositionOnGrid(t, "RRRR", "0:0:N", tenByTenGrid)
 }
 
 func TestWhenTheRoverMovesNorthThenItsPositionIncrements(t *testing.T) {
-	assertCommandYieldsPositionOnGrid(t, "M", "0:1:N", tenByTenGrid)
+	assertThatCommandYieldsPositionOnGrid(t, "M", "0:1:N", tenByTenGrid)
 }
 func TestWhenTheRoverMovesEastThenItsPositionIncrements(t *testing.T) {
-	assertCommandYieldsPositionOnGrid(t, "RM", "1:0:E", tenByTenGrid)
+	assertThatCommandYieldsPositionOnGrid(t, "RM", "1:0:E", tenByTenGrid)
 }
 func TestWhenTheRoverMovesSouthThenItsPositionDecrements(t *testing.T) {
-	assertCommandYieldsPositionOnGrid(t, "MRRM", "0:0:S", tenByTenGrid)
+	assertThatCommandYieldsPositionOnGrid(t, "MRRM", "0:0:S", tenByTenGrid)
 }
 
 func TestWhenTheRoverMovesWestThenItsPositionIncrements(t *testing.T) {
-	assertCommandYieldsPositionOnGrid(t, "RMRRM", "0:0:W", tenByTenGrid)
+	assertThatCommandYieldsPositionOnGrid(t, "RMRRM", "0:0:W", tenByTenGrid)
 }
 
 func TestWhenTheRoverReachesTheNorthSideOfTheGridThenItWrapsAround(t *testing.T) {
-	assertCommandYieldsPositionOnGrid(t, "MMMMMMMMMM", "0:0:N", tenByTenGrid)
+	assertThatCommandYieldsPositionOnGrid(t, "MMMMMMMMMM", "0:0:N", tenByTenGrid)
 }
 func TestWhenTheRoverReachesTheSouthSideOfTheGridThenItWrapsAround(t *testing.T) {
-	assertCommandYieldsPositionOnGrid(t, "LLM", "0:9:S", tenByTenGrid)
+	assertThatCommandYieldsPositionOnGrid(t, "LLM", "0:9:S", tenByTenGrid)
 }
 
 func TestWhenTheRoverReachesTheWestSideOfTheGridThenItWrapsAround(t *testing.T) {
-	assertCommandYieldsPositionOnGrid(t, "LM", "9:0:W", tenByTenGrid)
+	assertThatCommandYieldsPositionOnGrid(t, "LM", "9:0:W", tenByTenGrid)
 }
 
 func TestWhenTheRoverReachesTheEastSideOfTheGridThenItWrapsAround(t *testing.T) {
-	assertCommandYieldsPositionOnGrid(t, "RMMMMMMMMMM", "0:0:E", tenByTenGrid)
+	assertThatCommandYieldsPositionOnGrid(t, "RMMMMMMMMMM", "0:0:E", tenByTenGrid)
 }
